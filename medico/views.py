4d9d5388ff02c405c1e85cab54cf3ce02e19c721
Dival Lucas <lusca404@gmail.com>
2024-04-21 21:19:14 -0300

    open time of treatment

--- medico/views.py
+++ medico/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Especialidades, DadosMedico, is_medico
+from .models import Especialidades, DadosMedico, is_medico, DatasAbertas
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.contrib.auth.models import User
+from datetime import datetime
 
 def cadastro_medico(request):
 
@@ -60,3 +61,19 @@ def abrir_horario(request):
         dados_medicos = DadosMedico.objects.get(user=request.user)
         return render(request, 'abrir_horario.html', {'dados_medicos': dados_medicos})
     elif request.method == "POST":
+        data = request.POST.get('data ')
+        data_formatada = datetime.strptime(data, "%Y-%m-%dT%H:%M")
+
+    if data_formatada <= datetime.now():
+            messages.add_message(request, constants.WARNING, 'A data deve ser maior ou igual a data atual.')
+            return redirect('/medicos/abrir_horario')
+    
+    horario_abrir = DatasAbertas(
+        data=data,
+        user=request.user
+    )
+    
+    horario_abrir.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Horário cadastrado com sucesso.')
+    return redirect('/medicos/abrir_horario')
